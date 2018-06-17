8c7e09a90af6c64236dd03479c230ed59e788cc6
spirit <1020186996@163.com>
2018-06-17 17:25:13 +0800

    格式化test

--- manage.py
+++ manage.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/index')
 def index():
-    return 'index'
+	return 'index'
+
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
