5508a59dbf15478a19a4dfa61f9d7dbf7e2e5b7b
spirit <1020186996@163.com>
2018-06-19 20:48:50 +0800

    config2

--- config.py
+++ config.py
@@ -1,2 +1,33 @@
+import redis
+from flask import logging
+
+
 class Config:
-	pass
+
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:w92z12x14@127.0.0.1:3306/information"
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	REDIS_HOST = "127.0.0.1"
+	REDIS_PORT = 6379
+	SECRET_KEY = 'aa'
+	SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
+	SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+	SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
+	PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+	LOG_LEVEL = logging.DEBUG
+
+
+class DevelopementConfig(Config):
+	"""开发模式下的配置"""
+	DEBUG = True
+
+
+class ProductionConfig(Config):
+	"""生产模式下的配置"""
+	LOG_LEVEL = logging.ERROR
+
+
+config = {
+	"development": DevelopementConfig,
+	"production": ProductionConfig
+}
--- manage.py
+++ manage.py
@@ -6,31 +6,13 @@ from flask_wtf import CSRFProtect
 from flask_session import Session
 from flask_migrate import Migrate, MigrateCommand
 
-app = Flask(__name__)
+from info import create_app, db
+app = create_app('development')
 
 
-class Config:
-	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:w92z12x14@127.0.0.1:3306/information"
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	REDIS_HOST = "127.0.0.1"
-	REDIS_PORT = 6379
-	SECRET_KEY = 'aa'
-	SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
-	SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-	SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-	PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
-
-
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-# redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-manager = Manager(app)
-csrf = CSRFProtect(app)
-Session(app)
 migrate = Migrate(app)
 Migrate(app, db)
+manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
