bddc14658dd1021a5b959604eb71f5950206d4a2
spirit <1020186996@163.com>
2018-06-25 23:31:58 +0800

    解决一些bug

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,8 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, g
 
 from info.modules.profile import profile_blu
+from info.utils.common import user_login_data
 
 
 @profile_blu.route('/info')
+@user_login_data
 def user_info():
-	return render_template('news/user.html')
+
+	user = g.user
+	if not user:
+		# 用户未登录，重定向到主页
+		return redirect('/')
+
+	data = {
+		"user": user.to_dict(),
+	}
+	# 渲染模板
+	return render_template("news/user.html", data=data)
