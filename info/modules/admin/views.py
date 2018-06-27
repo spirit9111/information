ae6560f0f7bbc4c0e21ad18ddc5aa669df42a207
spirit <1020186996@163.com>
2018-06-27 15:06:36 +0800

    后台登录错误提示功能  bug content 取不到

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -15,24 +15,24 @@ def login():
 	password = request.form.get('password')
 	# 判空
 	if not all([nick_name, password]):
-		return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+		return render_template('admin/login.html', errmsg="参数错误")
 	# 校验
 	# 判断user是否存在
 	try:
 		user_mo = User.query.filter(User.nick_name == nick_name).first()
 	except Exception as e:
 		current_app.logger.debug(e)
-		return jsonify(errno=RET.DBERR, errmsg="数据库异常")
+		return render_template('admin/login.html', errmsg="数据库异常")
 
 	if not user_mo:
-		return jsonify(errno=RET.USERERR, errmsg="用户不存在")
+		return render_template('admin/login.html', errmsg="用户不存在")
 
 	# 判断pwd是否正确
 	if not user_mo.check_password(password):
-		return jsonify(errno=RET.PWDERR, errmsg="密码错误")
+		return render_template('admin/login.html', errmsg="密码错误")
 	# 判断权限
 	if not user_mo.is_admin:
-		return jsonify(errno=RET.LOGINERR, errmsg="权限错误")
+		return render_template('admin/login.html', errmsg="权限错误")
 	# 保持登录状态
 	session['nick_name'] = user_mo.nick_name
 	session['mobile'] = user_mo.mobile
