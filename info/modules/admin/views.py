ce8c6ed7f5d35027606b887ee9675d642c71cb20
spirit <1020186996@163.com>
2018-06-27 15:01:24 +0800

    后台登录功能  bug content 取不到

---
+++ info/modules/admin/views.py
@@ -0,0 +1,47 @@
+from flask import render_template, request, jsonify, redirect, current_app, url_for, session
+
+from info.models import User
+from info.modules.admin import admin_blu
+from info.utils.response_code import RET
+
+
+@admin_blu.route('/login', methods=['POST', 'GET'])
+def login():
+	"""登录"""
+	if request.method == 'GET':
+		return render_template('admin/login.html')
+	# 获取参数(name/pwd)
+	nick_name = request.form.get('username')
+	password = request.form.get('password')
+	# 判空
+	if not all([nick_name, password]):
+		return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+	# 校验
+	# 判断user是否存在
+	try:
+		user_mo = User.query.filter(User.nick_name == nick_name).first()
+	except Exception as e:
+		current_app.logger.debug(e)
+		return jsonify(errno=RET.DBERR, errmsg="数据库异常")
+
+	if not user_mo:
+		return jsonify(errno=RET.USERERR, errmsg="用户不存在")
+
+	# 判断pwd是否正确
+	if not user_mo.check_password(password):
+		return jsonify(errno=RET.PWDERR, errmsg="密码错误")
+	# 判断权限
+	if not user_mo.is_admin:
+		return jsonify(errno=RET.LOGINERR, errmsg="权限错误")
+	# 保持登录状态
+	session['nick_name'] = user_mo.nick_name
+	session['mobile'] = user_mo.mobile
+	session['user_id'] = user_mo.id
+
+	# 重定向到admin/index
+	return redirect(url_for('admin.index'))
+
+
+@admin_blu.route('/index')
+def index():
+	return render_template('admin/index.html')
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db, models
+from info.models import User
 
 app = create_app('development')
 # app = create_app('production')
@@ -10,7 +11,30 @@ Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
+# 创建管理员账号的命令
+@manager.option('-n', '-name', dest='name')
+@manager.option('-p', '-password', dest='password')
+def createsuperuser(name, password):
+	"""创建管理员账号"""
+	if not all([name, password]):
+		print('参数不足')
+		return
+	user = User()
+	user.nick_name = name
+	user.mobile = name
+	user.password = password
+	user.is_admin = True
+	try:
+		db.session.add(user)
+		db.session.commit()
+		print("OK!")
+	except Exception as e:
+		print(e)
+		db.session.rollback()
+
+
 if __name__ == '__main__':
 	manager.run()
 
-# redis-server /etc/redis/redis.conf 开启服务器
+# redis-server /etc/redis/redis.conf 开启redis服务器
